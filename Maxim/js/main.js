b0559bdc395aebf68df29996429ab85038b48d8d
marianamendivil <mariana.mendidu@gmail.com>
2018-11-19 01:26:58 -0500

    json info

--- Maxim/js/main.js
+++ Maxim/js/main.js
@@ -15,19 +15,23 @@ var path = new URLSearchParams(window.location.search);
 getAjax("https://www.googleapis.com/books/v1/volumes?q=:" + (path.get('subject')) + "&key=AIzaSyCbZ8Bgts91KrvGCds4KNtf_7rPaT3zhe0", function (data) {
     books = JSON.parse(data)['items'];
     books.forEach(book => {
-        var node = document.createElement("option");
-        node.innerHTML = book["volumeInfo"]["title"];
+        var title = book["volumeInfo"]["title"];
         var authors = book["volumeInfo"]["authors"];
+        var authorS = "";
         if (authors != null) {
             authors.forEach(author => {
-
-                node.innerHTML = author;
+                authorS +=  author + ", ";
             });
         }
-        node.innerHTML = book["volumeInfo"]["publisher"];
-        node.innerHTML = book["volumeInfo"]["publishedDate"];
-        node.innerHTML = book["volumeInfo"]["description"];
-        document.getElementById('fantasy').appendChild(node);
-        console.log(path.get('subject'))
+        var publisher = book["volumeInfo"]["publisher"];
+        var publishedDate = book["volumeInfo"]["publishedDate"];
+        var description = book["volumeInfo"]["description"];
+
+        var str = "<div class='col-md-4'>";
+        str += "<div class='card'><img class='card-img-top' src='' alt='card image cap' /></div><div class='card-body'><p>"+(title)+"</p><p>"+(publishedDate)+"</p><p>"+(publisher)+"</p><p>"+(description)+"</p><p>"+(authors)+"</p></div>";
+        str += "</div>";
+        document.getElementById("rowbebe").innerHTML = str;
+        // document.getElementByClass('card-body').innerHTML(`${}`);
+        //console.log(path.get('subject'))
     });
 });
