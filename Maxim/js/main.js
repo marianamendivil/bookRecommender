e4a751ecbcb379266c13adccd5411b7636acede2
juanguzmant <juancamiloguzmant36@gmail.com>
2018-11-19 00:27:10 -0500

    no se

--- Maxim/js/main.js
+++ Maxim/js/main.js
@@ -1,8 +1,8 @@
 function getAjax(url, success) {
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
@@ -10,11 +10,24 @@ function getAjax(url, success) {
 }
 
 // example request
-getAjax('https://www.googleapis.com/books/v1/volumes?q=subject:fantasy&key=AIzaSyCbZ8Bgts91KrvGCds4KNtf_7rPaT3zhe0', function(data){
+var path = new URLSearchParams(window.location.search);
+
+getAjax("https://www.googleapis.com/books/v1/volumes?q=:" + (path.get('subject')) + "&key=AIzaSyCbZ8Bgts91KrvGCds4KNtf_7rPaT3zhe0", function (data) {
     books = JSON.parse(data)['items'];
     books.forEach(book => {
         var node = document.createElement("option");
         node.innerHTML = book["volumeInfo"]["title"];
+        var authors = book["volumeInfo"]["authors"];
+        if (authors != null) {
+            authors.forEach(author => {
+
+                node.innerHTML = author;
+            });
+        }
+        node.innerHTML = book["volumeInfo"]["publisher"];
+        node.innerHTML = book["volumeInfo"]["publishedDate"];
+        node.innerHTML = book["volumeInfo"]["description"];
         document.getElementById('fantasy').appendChild(node);
+        console.log(path.get('subject'))
     });
 });
